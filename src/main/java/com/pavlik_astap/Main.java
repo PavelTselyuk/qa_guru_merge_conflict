439448ecfab2ea167a646a6a5940d4c6bf102ca3
PavelTselyuk <56223381+PavelTselyuk@users.noreply.github.com>
2026-07-13 01:52:55 +0300

    changed greetings

--- src/main/java/com/pavlik_astap/Main.java
+++ src/main/java/com/pavlik_astap/Main.java
@@ -5,7 +5,7 @@ package com.pavlik_astap;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome, sweetheart!");
+        System.out.println("Hello and welcome, student!");
 
     }
 }
